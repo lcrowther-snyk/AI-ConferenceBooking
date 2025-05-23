26e9420bc485f1f11794c8af003b3ab75fc9be78
lawrencecrowther <loz@snyk.io>
2025-05-23 11:40:54 +1000

    added search feature

--- src/main/java/org/workshop/aiconferencebooking/controller/HomeController.java
+++ src/main/java/org/workshop/aiconferencebooking/controller/HomeController.java
@@ -47,6 +47,13 @@ public class HomeController {
         return "index";
     }
     public List<Talk> searchTalk(String input) {
-        return null;
+        //to lowercase     the input
+        var lowerInput = input.toLowerCase();
+        //create a query to search for talks
+        var query = em.createNativeQuery("select t.* from Talk t join Person p on t.speaker_id = p.id where lower(t.description) like '%" + lowerInput + "%' OR lower(t.title) like '%" + lowerInput + "%' OR lower(p.username) like '%" + lowerInput + "%'", Talk.class);
+        //execute the query
+        var talks = (List<Talk>) query.getResultList();
+        //return the result
+        return talks;
     }
 }
